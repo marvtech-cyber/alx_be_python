249ac4b2241734c40cdd12db57505daaeace9365
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-06-21 06:54:20 +0300

    fixed variable assignment

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,9 @@
 #a python script for calculating simple interest
-P = 1000    # Principle amount
-R = 0.05    # Rate of interest 5%
-T = 3       # Time value in years
+Principal = 1000    # Principle amount
+Rate = 0.05    # Rate of interest 5%
+Time = 3       # Time value in years
 
 # calculating interest
-I = P * R * T
+I = Principal * Rate * Time
 
 print(f"The simple inyterest is: {I}")
