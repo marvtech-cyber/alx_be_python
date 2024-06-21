48d59390ab068215e0137656aea93bc1592b9874
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-06-21 06:45:00 +0300

    a python script for calculating simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#a python script for calculating simple interest
+P = 1000    # Principle amount
+R = 0.05    # Rate of interest 5%
+T = 3       # Time value in years
+
+# calculating interest
+I = P * R * T
+
+print(f"The simple inyterest is: {I}")
