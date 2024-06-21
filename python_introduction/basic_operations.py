276b0784aec445f62a540645bdc45cdbff0bd29d
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-06-21 06:26:20 +0300

    basic operations code

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+minus = number1 - number2
+multiply = number1 * number2
+
+print(f"addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {minus}")
+print(f"Multiplication of {number1} and {number2} is {multiply}")
