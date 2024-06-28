a6cbd6d6d93610c9d357396376e791a51d3b47c8
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-06-28 13:47:49 +0300

    control flow assignment

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,19 @@
+'''
+Create a Python script named weather_advice.py. 
+This script will ask the user about the current weather conditions and provide clothing recommendations based on the input. 
+This task aims to demonstrate the use of if, elif, and else statements to make decisions in a program
+'''
+user_input = input("What's the weather like today? (sunny/rainy/cold): ")
+
+if user_input == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+
+elif user_input == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+
+elif user_input == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+
+else:
+    print("Sorry, I don't have recommendations for this weather.")
+
