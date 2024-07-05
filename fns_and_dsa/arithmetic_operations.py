b5fb216fb2a475cbd65f616907d648f77a0a8118
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-07-05 14:14:08 +0300

    fixed

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -13,19 +13,28 @@ t can recognize and display appropriately.
 Return the result of the arithmetic operation.
 '''
 
-def perform_operation( num1, num2, operation):
-    match operation:
-        case x if x == 'add':
-            return num1 + num2
-        
-        case x if x == 'subtract':
-            return num1 - num2
-        
-        case x if x == 'multiply':
-            return num1 * num2
-        
-        case 'division':
-            if num1 or num2 == 0:
-                return "Division by zero not allowed"
-            else:
-                return num1 / num2
+def perform_operation(num1: float, num2: float, operation: str) -> float:
+    """
+    Performs basic arithmetic operations.
+
+    Args:
+        num1 (float): The first number.
+        num2 (float): The second number.
+        operation (str): The operation to perform. Can be 'add', 'subtract', 'multiply', or 'divide'.
+
+    Returns:
+        float: The result of the arithmetic operation.
+    """
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        else:
+            return num1 / num2
+    else:
+        return "Error: Invalid operation."
