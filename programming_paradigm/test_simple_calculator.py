d44996859c75bee41ce76634c0ccecf3b5d6d0c3
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-07-14 12:05:13 +0300

    changed fuction name of both divide and multiply to multiplication and division

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,11 +12,11 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 2), 4)
         
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 5), 2)
         self.assertEqual(self.calc.divide(10, 0), None)
 
