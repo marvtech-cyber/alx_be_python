6c1699d56d0da5b4a06f8cbde2d3cd18003cacb0
marvin nyarenchi <marvin.nyarenchi@github.com>
2024-07-21 12:52:58 +0300

    changed printing technique in library class

--- oop/library_system.py
+++ oop/library_system.py
@@ -51,5 +51,10 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book)
+            if isinstance(book, EBook):
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
+            elif isinstance(book, PrintBook):
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
+            else:
+                print(f"Book: {book.title} by {book.author}")
 
